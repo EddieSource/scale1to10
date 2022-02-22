209d5565caf51f003a1fa250ee275006fe786925
Your Name <youremail@domain.com>
2022-02-21 21:21:12 -0500

    NavBar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from "./logo.svg";
-import LoginPage from "./pages/LoginPage";
+//import logo from "./logo.svg";
 import { Link, Outlet } from "react-router-dom";
 import "./App.css";
 import ExplorationPage from "./pages/GalleryPage";
 import Grid from "@mui/material/Grid";
 
+//import ProfilePage from "./pages/ProfilePage";
+//import LoginPage from "./pages/LoginPage";
 const App = () => {
   return (
     <div className="App">
@@ -24,7 +25,9 @@ const App = () => {
           borderBottom: "solid 1px",
           paddingBottom: "1rem",
         }}
-      ></nav>
+      >
+        <Link to="/login">login</Link> | <Link to="/signup">signup</Link>|<Link to="/profile">profile</Link>
+      </nav>
 
       <Outlet />
     </div>
--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -1,7 +1,21 @@
-import React from "react";
-
-const ProfilePage = () => {
-  return <div>Profile</div>;
-};
-
-export default ProfilePage;
+import { NavBar } from '../components';
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+const profilePage = () =>{
+  //Style Cosmic Latte!
+  //Page Structure: NavBar, Container{Content}
+  return (
+    <div>
+      <div>
+        < NavBar/>
+      </div>
+      <div>
+        <Container maxWidth="lg">
+        <Box sx={{ bgcolor: '#FFF8E7', height: '100vh' }} />
+      </Container>
+      </div>
+    </div>
+  );
+}
+export default profilePage;
