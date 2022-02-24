c8010dcc37ba81270306fe8fe6c8255824a7a093
Xianhui Zhu <xz2715@nyu.edu>
2022-02-23 20:38:09 -0500

    fix some bugs

--- src/App.js
+++ src/App.js
@@ -11,17 +11,7 @@ import PrimarySearchAppBar from "./components/NavBar";
 const App = () => {
   return (
     <div className="App">
-      <Grid container spacing={2}>
-        <Grid item xs={8}>
-          <h1> navbar </h1>
-        </Grid>
-        <Grid item xs={4}>
-          <Link to="/login">login</Link> | <Link to="/signup">signup</Link> |{" "}
-          <Link to="/profile">profile</Link>
-        </Grid>
-      </Grid>
       <PrimarySearchAppBar />
-      <ExplorationPage />
 
       <Outlet />
     </div>
